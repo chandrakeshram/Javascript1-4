968bbf8c95c9eeb4b05cf3d1ec204e1735b75aff
Chandrakesh Ram <chandrakeshram31@gmail.com>
2023-04-19 14:47:38 +0530

    Added content regarding operators and its types

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
 console.log("Hello WOrld");
+let str = "String ";
+console.log(str+10);
+
+
+console.log(typeof(str));
+
+
+const obj = {
+    "name":"Chandrakesh",
+    "age":22,
+    "hobby":"Cricket",
+    "isMale":true
+}
+console.log(obj['hobby']);
+const dictionary ={
+    "ashtonished":"surprised",
+    "connundrum":"confusing situation",
+    "pale":"bucket",
+    "swine":"pig",
+    "intermediate":"class 12th"
+}
+console.log(dictionary.pale);
+let a=32;
+let b=3;
+//Arithmetic Operators
+console.log("a + b "+ (a+b));
+console.log("a - b "+ (a-b));
+console.log("a x b "+ (a*b));
+console.log("a % b "+ (a%b));
+
+//Assignment Operators
+console.log(a+=b);
+console.log(a-=b);
+console.log(a*=b);
+console.log(a/=b);
+console.log(a**=b);
+console.log(a+=1);
+console.log(a);
+
+//Comparison Operators
+console.log(a==b);
+console.log(a!=b);
+console.log(a===b);
+console.log(a!==b);
+console.log(a>b);
+console.log(a<b);
+console.log(a<=b);
+console.log(a>=b);
+let res = (a>b)?a:b;
+console.log(res);
+
+//Logical Operators
+console.log(a>b && a!==b);
+console.log(a<b || a==b);
+console.log(!false);
