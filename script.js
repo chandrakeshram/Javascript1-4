12a81dfd2c5f422166676c4e96a7c2853ac0e18f
Chandrakesh Ram <chandrakeshram31@gmail.com>
2023-04-27 14:20:33 +0530

    Normal Commit

--- script.js
+++ script.js
@@ -1,57 +1,59 @@
-console.log("Hello WOrld");
-let str = "String ";
-console.log(str+10);
+// console.log("Hello WOrld");
+// let str = "String ";
+// console.log(str+10);
 
 
-console.log(typeof(str));
+// console.log(typeof(str));
 
 
-const obj = {
-    "name":"Chandrakesh",
-    "age":22,
-    "hobby":"Cricket",
-    "isMale":true
-}
-console.log(obj['hobby']);
-const dictionary ={
-    "ashtonished":"surprised",
-    "connundrum":"confusing situation",
-    "pale":"bucket",
-    "swine":"pig",
-    "intermediate":"class 12th"
-}
-console.log(dictionary.pale);
-let a=32;
-let b=3;
-//Arithmetic Operators
-console.log("a + b "+ (a+b));
-console.log("a - b "+ (a-b));
-console.log("a x b "+ (a*b));
-console.log("a % b "+ (a%b));
+// const obj = {
+//     "name":"Chandrakesh",
+//     "age":22,
+//     "hobby":"Cricket",
+//     "isMale":true
+// }
+// console.log(obj['hobby']);
+// const dictionary ={
+//     "ashtonished":"surprised",
+//     "connundrum":"confusing situation",
+//     "pale":"bucket",
+//     "swine":"pig",
+//     "intermediate":"class 12th"
+// }
+// console.log(dictionary.pale);
+// let a=32;
+// let b=3;
+// //Arithmetic Operators
+// console.log("a + b "+ (a+b));
+// console.log("a - b "+ (a-b));
+// console.log("a x b "+ (a*b));
+// console.log("a % b "+ (a%b));
 
-//Assignment Operators
-console.log(a+=b);
-console.log(a-=b);
-console.log(a*=b);
-console.log(a/=b);
-console.log(a**=b);
-console.log(a+=1);
-console.log(a);
+// //Assignment Operators
+// console.log(a+=b);
+// console.log(a-=b);
+// console.log(a*=b);
+// console.log(a/=b);
+// console.log(a**=b);
+// console.log(a+=1);
+// console.log(a);
 
-//Comparison Operators
-console.log(a==b);
-console.log(a!=b);
-console.log(a===b);
-console.log(a!==b);
-console.log(a>b);
-console.log(a<b);
-console.log(a<=b);
-console.log(a>=b);
-let res = (a>b)?a:b;
-console.log(res);
+// //Comparison Operators
+// console.log(a==b);
+// console.log(a!=b);
+// console.log(a===b);
+// console.log(a!==b);
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a<=b);
+// console.log(a>=b);
+// let res = (a>b)?a:b;
+// console.log(res);
 
-//Logical Operators
-console.log(a>b && a!==b);
-console.log(a<b || a==b);
-console.log(!false);
+// //Logical Operators
+// console.log(a>b && a!==b);
+// console.log(a<b || a==b);
+// console.log(!false);
 
+let ctitle = document.getElementsByClassName("card-title")[0];
+ctitle.style.color = "blue";
