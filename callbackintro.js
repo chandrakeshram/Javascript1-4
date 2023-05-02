ee7eda738e2eadf1a03dc975c77bd0d1c0859a68
Chandrakesh Ram <chandrakeshram31@gmail.com>
2023-05-02 21:36:13 +0530

    Added callback intro file

--- callbackintro.js
+++ callbackintro.js
@@ -17,11 +17,42 @@ console.log("End"); //Though it is written later in code still it will be execut
 function loadScript(src, tp){
     let script= document.createElement('script');
     script.src = src;
+    script.onload=()=>{
+        console.log("SRC "+script.src +" loaded successfully");
+        tp(null, src);
+    }
     document.body.append(script);
-    console.log("SRC "+script.src +" loaded successfully");
-    tp(src);
+    script.onerror=()=>{
+        console.error("SRC"+script.src +"cannot be loaded due to some error");
+        tp(new Error("Hey this is new error"));
+    }
 }
-const tp = (source)=>{
+const tp = (error, source)=>{
+    if(error){
+        console.error(error);
+    }
     console.log("Hey, I am Time Pass function "+source);
 }
-loadScript('https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js',tp);
+
+const original = (url, callback)=>{
+    let script1= document.createElement('script');
+    script1.src = url;
+    script1.onload=()=>{
+        console.log("Script with URL "+url+" loaded successfully");
+        callback(null, url);
+    }
+    script1.onerror=()=>{
+        console.error("Script with URL "+url+" did not loaded successfully");
+        callback(new Error("Error is occurred"));
+    }
+    document.body.append(script1);
+
+}
+const timpass=(error,url)=>{
+    if(error){
+        console.error(error);
+    }else{
+    console.log("Hum to hokar rahenge" ,url);}
+}
+original('https://cdn.jsdelivr.net/njkjkjhpm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js',timpass)
+// loadScript('https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js',tp);
