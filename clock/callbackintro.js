fc0de0d7e473165590872299dca22c53cb50ad66
Chandrakesh Ram <chandrakeshram31@gmail.com>
2023-05-02 20:34:09 +0530

    Added callback intro file

--- clock/callbackintro.js
+++ clock/callbackintro.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+//Synchronous Programming
+/*let a = prompt("Enter your name");
+let b = prompt("Enter your age");
+let c = prompt("Enter your marks");
+console.log(`${a} has ${b} age and has marks i.e ${c}`);*/
+
+//Asynchronous Programming 
